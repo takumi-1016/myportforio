c7620825375f152415af55d579f8002d97af7460
takumi-1016 <tn1016recruit@gmail.com>
2022-11-15 21:53:28 +0900

    掲示板機能の追加-tag機能-

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,6 @@
 class Tag < ApplicationRecord
+  has_many :tag_relationships, dependent: :destroy, foreign_key: 'tag_id'
+  has_many :post_titles, through: :tag_relationships
+
+  validates :name, uniqueness: true, presence: true
 end
