b8a464023e0feac0a48d0890e9b64e958e98e661
takumi-1016 <tn1016recruit@gmail.com>
2022-11-15 21:29:50 +0900

    掲示板機能の追加-model-

--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -1,2 +1,16 @@
 class PostComment < ApplicationRecord
+  validates :comment, presence: true
+
+  belongs_to :user
+  belongs_to :team
+  belongs_to :post_title
+  has_many :likes, dependent: :destroy
+  has_many :tag_relationships, dependent: :destroy
+  has_many :tags, through: :tag_relationships
+
+  default_scope -> { order(created_at: :desc) }
+
+  def liked_by?(user)
+    likes.where(user_id: user.id).exists?
+  end
 end
--- app/models/post_title.rb
+++ app/models/post_title.rb
@@ -1,2 +1,26 @@
 class PostTitle < ApplicationRecord
+  validates :title, presence: true
+
+  belongs_to :user
+  belongs_to :team
+  has_many :post_comments, dependent: :destroy
+  has_many :tag_relationships, dependent: :destroy
+  has_many :tags, through: :tag_relationships
+
+  default_scope -> { order(created_at: :desc) }
+
+  def save_tag(sent_tags)
+    current_tags = tags.pluck(:name) unless tags.nil?
+    old_tags = current_tags - sent_tags
+    new_tags = sent_tags - current_tags
+
+    old_tags.each do |old|
+      tags.delete Tag.find_by(name: old)
+    end
+
+    new_tags.each do |new|
+      new_post_tag = Tag.find_or_create_by(name: new)
+      tags << new_post_tag
+    end
+  end
 end
