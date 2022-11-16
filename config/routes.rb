2e7d9a799c406ec7c744f5fcc93ef271a2638537
takumi-1016 <tn1016recruit@gmail.com>
2022-11-16 13:46:51 +0900

    rails adminの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :tags do
     get 'search', to: 'post_titles#search'
   end
--- spec/factories/teams.rb
+++ spec/factories/teams.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :team do
     team_name { Faker::Lorem.characters(number: 5) }
-    certification_id {Faker::Lorem.characters(number: 5)}
+    certification_id { Faker::Lorem.characters(number: 5) }
   end
 end
