52eaa1fffe24ee265272a70120369956ddaf8826
takumi-1016 <tn1016recruit@gmail.com>
2022-11-16 15:40:48 +0900

    post_titleのmodelspec

--- spec/models/tag_relationship_spec.rb
+++ spec/models/tag_relationship_spec.rb
@@ -2,17 +2,17 @@ require 'rails_helper'
 
 RSpec.describe TagRelationship, type: :model do
   let(:tag_relationship) { create(:tag_relationship) }
-  
-  it "post_title_idとtag_idがある場合、有効であること" do
+
+  it 'post_title_idとtag_idがある場合、有効であること' do
     expect(tag_relationship).to be_valid
   end
 
-  it "post_title_idがない場合、無効であること" do
+  it 'post_title_idがない場合、無効であること' do
     tag_relationship.post_title_id = nil
     expect(tag_relationship).to be_invalid
   end
 
-  it "tag_idがない場合、無効であること" do
+  it 'tag_idがない場合、無効であること' do
     tag_relationship.tag_id = nil
     expect(tag_relationship).to be_invalid
   end
